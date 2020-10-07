81d49d97c9d40975fac9deab2454517e2aaf16d0
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 13:20:42 +0200

    RGB struggle session

--- src/main.zig
+++ src/main.zig
@@ -2,17 +2,20 @@ const std = @import("std");
 const dbg = @import("std").debug.print;
 
 const W = @TypeOf(std.io.getStdOut().writer());
+const I = i32;
+const RGB = struct { r: I, g: I, b: I };
 
-fn do_colors(w: W) !void {
-    const rgb = .{ 40, 80, 100 };
+fn do_colors(w: W, rgb: RGB) !void {
     try w.print("\x1b[38;2;{};{};{}m", rgb);
 }
 
 pub fn main() !void {
     const w = std.io.getStdOut().writer();
     dbg("is do debuggy things\n", .{});
-    try do_colors(w);
+    const rgb = .{ 40, 80, 100 };
+    try do_colors(w, rgb);
     try w.print("jamenhahåå {} \n", .{"gröt"});
 }
+
 // not a modeline, but a mapping:
-// noremap <Plug>ch:ir <cmd>update<cr>:!time zig build race<cr>
+// noremap <Plug>ch:ir <cmd>update<cr><cmd>call chansend(ww, "time zig build race\n")<cr>
