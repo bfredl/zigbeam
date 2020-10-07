cb43f1d79e5cf45838f4f725959a437197276114
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 11:57:28 +0200

    double the ooopsees<S-Del>

--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const dbg = @import("std").debug.print;
 const W = @TypeOf(std.io.getStdOut().writer());
 
 fn do_colors(w: W) !void {
-    try w.print("\x1b[1m", .{});
+    try w.print("\x1b[38;2;40;80;100m", .{});
 }
 
 pub fn main() !void {
