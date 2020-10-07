ffdadb4f558c15070a9d9c13a552ce10056507bf
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 14:23:15 +0200

    give up already :sob:

--- src/main.zig
+++ src/main.zig
@@ -6,13 +6,13 @@ const I = i32;
 const RGB = struct { r: I, g: I, b: I };
 
 fn do_colors(w: W, rgb: RGB) !void {
-    try w.print("\x1b[38;2;{};{};{}m", rgb);
+    try w.print("\x1b[38;2;{};{};{}m", .{ rgb.r, rgb.g, rgb.b });
 }
 
 pub fn main() !void {
     const w = std.io.getStdOut().writer();
     dbg("is do debuggy things\n", .{});
-    const rgb = .{ 40, 80, 100 };
+    const rgb = .{ .r = 40, .g = 80, .b = 100 };
     try do_colors(w, rgb);
     try w.print("jamenhahåå {} \n", .{"gröt"});
 }
