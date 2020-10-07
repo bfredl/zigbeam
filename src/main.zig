8de3da2310edf6a946097d6f77592fb2550151e0
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 14:26:17 +0200

    leave as you came

--- src/main.zig
+++ src/main.zig
@@ -9,12 +9,17 @@ fn do_colors(w: W, rgb: RGB) !void {
     try w.print("\x1b[38;2;{};{};{}m", .{ rgb.r, rgb.g, rgb.b });
 }
 
+fn reset(w: W) !void {
+    try w.print("\x1b[0m", .{});
+}
+
 pub fn main() !void {
     const w = std.io.getStdOut().writer();
     dbg("is do debuggy things\n", .{});
-    const rgb = .{ .r = 40, .g = 80, .b = 100 };
+    const rgb = .{ .r = 100, .g = 140, .b = 255 };
     try do_colors(w, rgb);
     try w.print("jamenhahåå {} \n", .{"gröt"});
+    try reset(w);
 }
 
 // not a modeline, but a mapping:
