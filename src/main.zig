cb394beb281a822a5b4131670bed8ace93fb98e7
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 10:33:01 +0200

    koka lite graut för fan

--- src/main.zig
+++ src/main.zig
@@ -2,5 +2,7 @@ const std = @import("std");
 const print = @import("std").debug.print;
 
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
     print("is do debuggy things\n", .{});
+    try stdout.print("jamenhahåå {} \n", .{"gröt"});
 }
