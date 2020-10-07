51d02167ff6446f7fd0be1699a29d6cc9cac937a
Björn Linse <bjorn.linse@gmail.com>
2020-10-07 10:41:31 +0200

    Shapes and colors are all I see

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,15 @@
 const std = @import("std");
 const print = @import("std").debug.print;
 
+const T = @TypeOf(std.io.getStdOut().writer());
+
+fn do_colors(w: T) !void {
+    try w.print("\x1bx", .{});
+}
+
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
+    const w = std.io.getStdOut().writer();
     print("is do debuggy things\n", .{});
-    try stdout.print("jamenhahåå {} \n", .{"gröt"});
+    try do_colors(w);
+    try w.print("jamenhahåå {} \n", .{"gröt"});
 }
